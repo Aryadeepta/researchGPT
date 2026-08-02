189a5830781ca31be3cb663049f609a7ef545f89
aryadeepta <aryadeeptade@gmail.com>
2026-08-02 09:32:21 -0400

    fix: enforce artifact writing in coder system instruction

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class ResearchOrchestrator:
         if self.project_dir:
             self.state["project_dir"] = self.project_dir
         self.planner = ResearchAgent("You are a research planner. Output JSON workflows.", model_queue=SMART_QUEUE)
-        self.coder = ResearchAgent("You are a Python coder. Output runnable code only.", model_queue=FAST_QUEUE)
+        self.coder = ResearchAgent("You are a Python coder. Output runnable code only. For every expected artifact, you MUST implement Python code to write it to disk using 'with open(filename, \"w\") as f: f.write(content)'. Do not omit this.", model_queue=FAST_QUEUE)
         self.adversary_board = AdversarialBoard()
         self.skills = {}
         self.stop_requested = False
